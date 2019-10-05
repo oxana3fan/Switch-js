36ccd530b738a5e9a3f44ae73b8115ed713d85cd
Coxik <coxik.coxik@gmail.com>
2019-10-05 11:29:39 +0300

    Final

---
+++ script.js
@@ -0,0 +1,50 @@
+var expr = 'days - of - week';
+
+switch (expr) {
+    case 'Monday':
+        alert('Working day');
+        break;
+    case 'Tuesday':
+        alert('Working day');
+        break;
+    case 'Wednesday':
+        alert('Working day');
+        break;
+    case 'Thursday':
+        alert('Working day');
+        break;
+    case 'Friday':
+        alert('Working day');
+        break;
+    case 'Saturday':
+        alert('Weekend');
+        break;
+    case 'Sunday':
+        alert('Weekend');
+        break;
+    default:
+        alert("Sorry " + " can not define, please try another value");
+
+
+        let (days - of - week) = prompt('Today is?', 'days - of - week');
+
+        if (days - of - week == 'Monday') {
+            alert('Working day');
+
+        } else if (days - of - week == 'Tuesday') {
+            alert('Working day');
+        } else if (days - of - week == 'Wendnesday') {
+            alert('Working day');
+        } else if (days - of - week == 'Thursday') {
+            alert('Working day');
+        } else if (days - of - week == 'Friday') {
+            alert('Working day');
+        } else if (days - of - week == 'Saturday') {
+            alert('Weekend');
+        } else if (days - of - week == 'Sunday') {
+            alert('Weekend');
+
+        } else {
+            alert('Can not define, please try another value');
+        }
+}
